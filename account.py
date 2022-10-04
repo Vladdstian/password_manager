9e74294841d2c7dd85923ffc819d0676a4dc1cd2
Vlad Nichifor <vlad.c.nichifor@gmail.com>
2022-10-04 12:32:14 +0300

    Account Class and password generating function updates

--- account.py
+++ account.py
@@ -7,11 +7,34 @@ LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
            'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 SYMBOLS = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+ACCOUNTS = "Password.csv"
+
+
+def generate_password(no_char):
+    max_letters = int(no_char * 0.5)
+    max_numbers = int(no_char * 0.25)
+    max_symbols = int(no_char * 0.25)
+    if max_symbols + max_numbers + max_letters != no_char:
+        max_letters += no_char - (max_symbols + max_numbers + max_letters)
+    password_letters = [random.choice(LETTERS) for _ in range(max_letters)]
+    password_numbers = [random.choice(NUMBERS) for _ in range(max_numbers)]
+    password_symbols = [random.choice(SYMBOLS) for _ in range(max_symbols)]
+    password_list = password_letters + password_numbers + password_symbols
+    random.shuffle(password_list)
+    password = "".join(password_list)
+    return password
 
 
 class Account:
     def __init__(self):
         self.account_details = self.create_account()
+
+    def create_account(self):
+        password = self.password_request()
+        website = input("Please enter the website for which you are creating an account: ")
+        username = input("Please enter the username/email address you want to use for the account: ")
+        account_details = {"website": website, "username": username, "password": password}
+        return account_details
 
     def password_request(self):
         password_choice = int(input("Would you like to (1)add a password or (2)generate one? "))
@@ -20,7 +43,7 @@ class Account:
             return password
         elif password_choice == 2:
             no_of_char = int(input("How many characters would you like your password to be? "))
-            password = self.generate_password(no_of_char)
+            password = generate_password(no_of_char)
             return password
         elif password_choice == 3:
             print("Thank you for using password service!")
@@ -29,28 +52,6 @@ class Account:
         else:
             print("I don't understand! Please try again or type (3) to cancel!\n\n")
             return self.password_request()
-
-    @staticmethod
-    def generate_password(no_char):
-        max_letters = int(no_char * 0.5)
-        max_numbers = int(no_char * 0.25)
-        max_symbols = int(no_char * 0.25)
-        if max_symbols + max_numbers + max_letters != no_char:
-            max_letters += no_char - (max_symbols + max_numbers + max_letters)
-        password_letters = [random.choice(LETTERS) for _ in range(max_letters)]
-        password_numbers = [random.choice(NUMBERS) for _ in range(max_numbers)]
-        password_symbols = [random.choice(SYMBOLS) for _ in range(max_symbols)]
-        password_list = password_letters + password_numbers + password_symbols
-        random.shuffle(password_list)
-        password = "".join(password_list)
-        return password
-
-    def create_account(self):
-        password = self.password_request()
-        website = input("Please enter the website for which you are creating an account: ")
-        username = input("Please enter the username/email address you want to use for the account: ")
-        account_details = {"website": website, "username": username, "password": password}
-        return account_details
 
     def save_account(self):
         for key in self.account_details:
@@ -61,12 +62,14 @@ class Account:
                 else:
                     self.account_details = {}
         if self.account_details != {}:
-            with open("Password.csv", "a", newline='') as file:
+            with open(ACCOUNTS, "a", newline='') as file:
                 writer = csv.writer(file)
-                if os.path.getsize("Password.csv") == 0:
+                if os.path.getsize(ACCOUNTS) == 0:
                     column_names = ["website", "username", "password"]
                     writer.writerow(column_names)
                 else:
                     pass
         else:
             pass
+
+    # save account to be moved into the gmail account
